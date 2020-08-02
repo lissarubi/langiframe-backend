6fa9627f7a82d45b745f5b2150d42627ffb74f7f
Ederson Ferreira <edersondeveloper@gmail.com>
2020-08-02 15:02:06 -0400

    fixing cors

--- src/controllers/printController.js
+++ src/controllers/printController.js
@@ -10,12 +10,7 @@ module.exports = {
             await page.goto(pageUrl);
             await page.setViewport({ width: 1000, height: 1000});
             const screenshot = await page.screenshot({ encoding: 'base64', fullPage: true });
-	    response.writeHead(200, {
-            	'Content-Type': 'image/png',
-        	'Content-Length': screenshot.length,
-                'Access-Control-Allow-Origin:': 'http://langiframe.herokuapp.com/'
-    	    });
-            response.end(screenshot);
+            response.json({ screenshot })};
 
             await browser.close();
         })();
