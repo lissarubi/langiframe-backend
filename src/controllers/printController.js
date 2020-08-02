4b9c0c6723c3bc92d2f655c52d79ff55964964f6
Ederson Ferreira <edersondeveloper@gmail.com>
2020-08-02 16:41:16 -0400

    fixing

--- src/controllers/printController.js
+++ src/controllers/printController.js
@@ -2,24 +2,18 @@ const puppeteer = require('puppeteer')
 
 module.exports = {
     async create(request, response){
-        const { pageUrl } = request.body;
         
         (async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(pageUrl);
-            await page.setViewport({ width: 1000, height: 1000});
-            const screenshot = await page.screenshot({ encoding: 'base64', fullPage: true });
-            response.json({ screenshot });
-
-	    response.writeHead(200, {
-            	'Content-Type': 'image/png',
-            	'Content-Length': screenshot.length,
-                "Access-Control-Allow-Origin": "http://langiframe.herokuapp.com/",
-                "Access-Control-Allow-Methods": 'POST,GET',
-    	    });
-
-            await browser.close();
+            try{
+                const { pageUrl } = request.body;
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto(pageUrl);
+                await page.setViewport({ width: 1000, height: 1000});
+                const screenshot = await page.screenshot({ encoding: 'base64', fullPage: true })
+                response.json({ screenshot })
+                await browser.close();
+            }catch(err){console.log(err)}
         })();
     }
 }
