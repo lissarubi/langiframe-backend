85b9cb165ebd1935200a55b78b6c6f44fd2bc34e
Ederson Ferreira <edersondeveloper@gmail.com>
2020-08-02 16:54:53 -0400

    fixing

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const execController = require('./controllers/execController')
-const printController = require('./controllers/printController')
-
 
 routes.post('/exec', execController.create)
-routes.post('/print', printController.create)
 
 module.exports = routes
